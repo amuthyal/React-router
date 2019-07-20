11050256b13d3dc20093d4f9a68e1edb067659ba
akhila <amuthyal@asu.edu>
2019-07-19 23:17:16 -0700

    Contain state by using Link Component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,32 +1,32 @@
 import React, { Component } from 'react';
 import Posts from '../../containers/Blog/Posts/Posts';
 // import axios from '../../axios';
-
+import {Route, Link} from 'react-router-dom';
+import NewPost from '../Blog/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    
-
-    
-    
-
     render () {
-       
-         
-
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home </a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home </Link> </li>
+                            <li><Link to={{
+                                pathname: "/new-post",
+                                // hash: '#submit',
+                                // search: '?quick-submit=true'
+                            }}> New Post</Link></li>
 
                         </ul>
                     </nav>
                 </header>
                 
-              <Posts/>
+              {/* <Route path="/" exact render = {() => <h1> Home </h1>}/> */}
+              <Route path="/" exact component = {Posts} />
+              <Route path="/new-post" component = {NewPost} />
+
             </div>
         );
     }
